b6d32b12cf8896ea18d8194f51ec6deee5c7cfb3
Max Zernyaev <LentyaiPrOO@yandex.ru>
2022-01-26 14:35:17 +0300

    this commit configures basic db connection, entity, migration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello world')
+import 'dotenv/config'
+
+import { MikroORM } from '@mikro-orm/core'
+import microConfig from './mikro-orm.config'
+
+import { Post } from './entities'
+
+const main = async () => {
+	const orm = await MikroORM.init(microConfig)
+	await orm.getMigrator().up()
+	const post = orm.em.create(Post, {
+		title: 'my first post'
+	})
+	await orm.em.persistAndFlush(post)
+
+	const posts = await orm.em.find(Post, {})
+	console.log(posts)
+}
+
+main().then()
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,20 @@
+import path from 'path'
+import { MikroORM } from '@mikro-orm/core'
+
+import Post from './entities/Post'
+import { __prod__ } from './constants'
+
+export default {
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	dbName: process.env.DB_NAME,
+	type: 'postgresql',
+	migrations: {
+		path: path.join(__dirname, './migrations'),
+		pattern: /^[\w-]+\d+\.[tj]s$/
+	},
+	entities: [Post],
+	debug: !__prod__
+} as Parameters<typeof MikroORM.init>[0]
