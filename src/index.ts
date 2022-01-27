84ac93ba7043088e459d137bb1dbab803fa9a090
Max Zernyaev <LentyaiPrOO@yandex.ru>
2022-01-27 20:35:18 +0300

    added basic crud for post with graphql

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,37 @@
 import 'dotenv/config'
+import 'reflect-metadata'
 
 import { MikroORM } from '@mikro-orm/core'
 import microConfig from './mikro-orm.config'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
 
-import { Post } from './entities'
+import { PostResolver } from './resolvers/post'
 
 const main = async () => {
 	const orm = await MikroORM.init(microConfig)
 	await orm.getMigrator().up()
-	const post = orm.em.create(Post, {
-		title: 'my first post'
-	})
-	await orm.em.persistAndFlush(post)
 
-	const posts = await orm.em.find(Post, {})
-	console.log(posts)
+	const app = express()
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [PostResolver],
+			validate: false
+		}),
+		context: () => ({em: orm.em})
+	})
+
+	await apolloServer.start()
+	apolloServer.applyMiddleware({app})
+	app.get('/', (_, res) => {
+		res.send('Hello')
+	})
+	const port = process.env.BACKEND_PORT || 4000
+
+	app.listen(port, () => {
+		console.log(`Listening at port:${port} 🚀🚀🚀`)
+	})
 }
 
 main().then()
